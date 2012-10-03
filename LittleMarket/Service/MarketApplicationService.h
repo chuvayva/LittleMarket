311989178ab4dbf58e7775f8f6fec3618304ec1b
chuvayva <chuvayva@gmail.com>
2012-10-03 23:36:07 +0400

    all tests success

---
+++ LittleMarket/Service/MarketApplicationService.h
@@ -0,0 +1,13 @@
+//
+//  MarketApplicationService.h
+//  LittleMarket
+//
+//  Created by BiT on 03.10.12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MarketApplicationService : NSObject
+
+@end
---
+++ LittleMarket/Service/MarketApplicationService.m
@@ -0,0 +1,13 @@
+//
+//  MarketApplicationService.m
+//  LittleMarket
+//
+//  Created by BiT on 03.10.12.
+//
+//
+
+#import "MarketApplicationService.h"
+
+@implementation MarketApplicationService
+
+@end
