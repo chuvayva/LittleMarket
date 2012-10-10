b46f579ba2689eb204da9a40e576a7406f19f3c7
chuvayva <chuvayva@gmail.com>
2012-10-11 00:58:17 +0400

    test

---
+++ LittleMarketTest/MarketFAil.h
@@ -0,0 +1,13 @@
+//
+//  MarketFAil.h
+//  LittleMarket
+//
+//  Created by BiT on 11.10.12.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MarketFAil : SenTestCase
+
+@end
---
+++ LittleMarketTest/MarketFAil.m
@@ -0,0 +1,18 @@
+//
+//  MarketFAil.m
+//  LittleMarket
+//
+//  Created by BiT on 11.10.12.
+//
+//
+
+#import "MarketFAil.h"
+
+@implementation MarketFAil
+
+-(void)testFail
+{
+    STFail(@"test for teamcity");
+}
+
+@end
